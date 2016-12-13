d8800d5e381cb2a637e2d20264b43d70ee080cbe
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-12-13 02:25:07 +0100

    Huge refactor and updates
    - Node holds weak_ptr's to other Nodes
    - VirusGenealogy functions updated to use new Node implementation
    - Recursive remove() implementation
    - create() now uses connect()
    - Some exception guarantee analysis
    - Refactor exception throwing into helper functions
    - Refactor whitespace
    - Rename variables

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -1,168 +1,208 @@
 #ifndef VIRUS_GENEALOGY_H
 #define VIRUS_GENEALOGY_H
 
-#include <vector>
 #include <map>
-#include <utility>
 #include <memory>
+#include <set>
+#include <utility>
+#include <vector>
 
 class VirusNotFound : std::exception {};
 class VirusAlreadyCreated : std::exception {};
 class TriedToRemoveStemVirus : std::exception {};
 
-
-
-
 template<class Virus>
 class VirusGenealogy{
-	private:
-		class Node{
-			public:
-				typename Virus::id_type id_;
-                Virus virus_;
+private:
+    class Node;
 
-				std::vector<typename Virus::id_type> children;
-				std::vector<typename Virus::id_type> parents;
+    typename Virus::id_type stem_id_;
+    std::map<typename Virus::id_type, std::shared_ptr<Node>> genealogy_;
 
-				Node(typename Virus::id_type id) : id_(id),
-                                                   virus_(Virus(id)) {};
-						
-				void add_child(typename Virus::id_type const &child_id) {
-					children.push_back(child_id); //strong
-				}
-						
-				void add_parent(typename Virus::id_type const &parent_id) {
-					parents.push_back(parent_id); // strong
-				}
-						
-				void add_parents(std::vector<typename Virus::id_type> const &parent_ids) {
-					// przy założeniu, że konstruktor kopiujący typename Virus::id_type jest no throw
-					// bedzie rzucac, trzeba zmodyfikowac
-					parents.insert(parents.end(), parent_ids.begin(), parent_ids.end());
-				}
-				// chyba trzeba dopisać konstruktory i = żeby były nothrow/strong
-				// == na virusie tez moze rzucac wyjatek
-				// wszystko co moze bedzie rzucac
-				// nie kopiowac mapy, mozna w nodzie trzymac jakis iterator wskazujacy miejsce
-				// nawet iterowanie po wektorze może rzucić wyjątkiem
-				// ale można tutaj trzymać iterator do miejsca w mapie
-				// można kopiować parents/children ale dużej mapy nie
-		};
-		
-		std::map<typename Virus::id_type, std::shared_ptr<Node>> mapa;
-		typename Virus::id_type first_id;
-		
-	public:
-		// Tworzy nową genealogię.
-		// Tworzy także węzeł wirusa macierzystego o identyfikatorze stem_id.
-		VirusGenealogy(typename Virus::id_type const &stem_id)
-			:first_id(stem_id) {
-			mapa.insert(make_pair(stem_id, std::make_shared<Node>(stem_id))); // strong
-		};
+    void throwIfNotFound(typename Virus::id_type const &id) const {
+        if (!exists(id)) {
+            throw VirusNotFound();
+        }
+    }
 
-		// Zwraca identyfikator wirusa macierzystego.
-		typename Virus::id_type get_stem_id() const {
-			return first_id; // nothrow
-		}
+    void throwIfAlreadyCreated(typename Virus::id_type const &id) const {
+        if (exists(id)) {
+            throw VirusAlreadyCreated();
+        }
+    }
 
-		// Zwraca listę identyfikatorów bezpośrednich następników wirusa
-		// o podanym identyfikatorze.
-		// Zgłasza wyjątek VirusNotFound, jeśli dany wirus nie istnieje.
-		std::vector<typename Virus::id_type> get_children(typename Virus::id_type const &id) const {
-			if (!exists(id))
-				throw VirusNotFound();
-				
-			std::shared_ptr<Node> ptr = mapa.find(id)->second; // strong
-			return ptr->children; // nothrow
-			
-		}
-
-		// Zwraca listę identyfikatorów bezpośrednich poprzedników wirusa
-		// o podanym identyfikatorze.
-		// Zgłasza wyjątek VirusNotFound, jeśli dany wirus nie istnieje.
-		std::vector<typename Virus::id_type> get_parents(typename Virus::id_type const &id) const {
-			if (!exists(id))
-				throw VirusNotFound();
-				
-			std::shared_ptr<Node> ptr = mapa.find(id)->second; // strong
-			return ptr->parents; // nothrow?
-		}
-
-		// Sprawdza, czy wirus o podanym identyfikatorze istnieje.
-		bool exists(typename Virus::id_type const &id) const {
-			return mapa.find(id) != mapa.end(); // strong
-		}
-
-		// Zwraca referencję do obiektu reprezentującego wirus o podanym
-		// identyfikatorze.
-		// Zgłasza wyjątek VirusNotFound, jeśli żądany wirus nie istnieje.
-		Virus& operator[](typename Virus::id_type const &id) const {
-            if (!exists(id))
-                throw VirusNotFound();
-
-            auto iterator = mapa.find(id);
-            std::shared_ptr<Node> node_sp = std::get<1>(*iterator);
-            Node& node = *(node_sp.get());
-            return node.virus_;
+    class Node {
+    public:
+        Node(typename Virus::id_type const &id) : virus_(id), id_(id) {
         }
 
-		// Tworzy węzeł reprezentujący nowy wirus o identyfikatorze id
-		// powstały z wirusów o podanym identyfikatorze parent_id lub
-		// podanych identyfikatorach parent_ids.
-		// Zgłasza wyjątek VirusAlreadyCreated, jeśli wirus o identyfikatorze
-		// id już istnieje.
-		// Zgłasza wyjątek VirusNotFound, jeśli któryś z wyspecyfikowanych
-		// poprzedników nie istnieje.
-		void create(typename Virus::id_type const &id, typename Virus::id_type const &parent_id) {
-			if (exists(id))
-				throw VirusAlreadyCreated();
-			if (!exists(parent_id))
-				throw VirusNotFound();
-	
-			std::shared_ptr<Node> parent = mapa.find(parent_id)->second; // strong
-			std::shared_ptr<Node> node =
-				std::make_shared<Node>(id);
-			node->add_parent(parent_id); // strong
-			mapa.insert(make_pair(id, node)); // strong
-			parent->add_child(id); // both strong
-		}
-		
-		void create(typename Virus::id_type const &id, std::vector<typename Virus::id_type> const &parent_ids) {
-			if (exists(id))
-				throw VirusAlreadyCreated();
-				
-			for (typename Virus::id_type parent_id : parent_ids)
-				if (!exists(parent_id))
-					throw VirusNotFound();
-				
-			std::shared_ptr<Node>
-				node = std::make_shared<Node>(id);
-			node->add_parents(parent_ids); // strong
-			mapa.insert(make_pair(id, node)); // strong
-			for (typename Virus::id_type parent_id : parent_ids) {
-				std::shared_ptr<Node> parent = mapa.find(parent_id)->second; // strong
-				parent->add_child(id); // strong
-			}
-		}
+        void add_child(std::shared_ptr<Node> &child) {
+            std::weak_ptr<Node> child_wp(child); // const
+            children_.insert(child_wp); // strong
+        } // therefore strong
 
-		// Dodaje nową krawędź w grafie genealogii.
-		// Zgłasza wyjątek VirusNotFound, jeśli któryś z podanych wirusów nie istnieje.
-		void connect(typename Virus::id_type const &child_id, typename Virus::id_type const &parent_id) {
-			if (!exists(parent_id) || !exists(child_id))
-				throw VirusNotFound();
-			
-			std::shared_ptr<Node> parent = mapa.find(parent_id)->second; // strong
-			std::shared_ptr<Node> child = mapa.find(child_id)->second; // strong
-				
-			parent->add_child(child_id); // strong
-			child->add_parent(parent_id); // strong
-		}
+        void add_parent(std::shared_ptr<Node> &parent) {
+            std::weak_ptr<Node> parent_wp(parent); //const
+            parents_.insert(parent_wp); // strong
+        } // therefore strong
 
-		// Usuwa wirus o podanym identyfikatorze.
-		// Zgłasza wyjątek VirusNotFound, jeśli żądany wirus nie istnieje.
-		// Zgłasza wyjątek TriedToRemoveStemVirus przy próbie usunięcia
-		// wirusa macierzystego.
-		// set zamiast wektora żeby usuwać szybciej?
-		void remove(typename Virus::id_type const &id);
+        void remove_child(std::shared_ptr<Node> &child) {
+            std::weak_ptr<Node> child_wp(child); // no-throw
+            children_.erase(child_wp); // no-throw
+        } // therefore no-throw
+
+        void remove_parent(std::shared_ptr<Node> &parent) {
+            std::weak_ptr<Node> parent_wp(parent); // no-throw
+            parents_.erase(parent_wp); // no-throw
+        } // therefore no-throw
+
+        std::vector<typename Virus::id_type> get_children() const {
+            std::vector<typename Virus::id_type> children_ids;
+
+            for (std::weak_ptr<Node> node_wp : children_) {
+                std::shared_ptr<Node> node_sp = node_wp.lock();
+                children_ids.push_back(node_sp->id_);
+            }
+
+            return children_ids;
+        }
+
+        std::vector<typename Virus::id_type> get_parents() const {
+            std::vector<typename Virus::id_type> parent_ids;
+
+            for (std::weak_ptr<Node> node_wp : parents_) {
+                std::shared_ptr<Node> node_sp = node_wp.lock();
+                parent_ids.push_back(node_sp->id_);
+            }
+
+            return parent_ids;
+        }
+
+        Virus& get_virus() {
+            return virus_;
+        }
+
+        typename Virus::id_type get_id() {
+            return id_;
+        }
+    private:
+        Virus virus_;
+        typename Virus::id_type id_;
+
+        std::set<std::weak_ptr<Node>, std::owner_less<std::weak_ptr<Node>>> children_;
+        std::set<std::weak_ptr<Node>, std::owner_less<std::weak_ptr<Node>>> parents_;
+    };
+public:
+    VirusGenealogy(typename Virus::id_type const &stem_id) : stem_id_(stem_id) {
+        std::shared_ptr<Node> node_sp = std::make_shared<Node>(Node(stem_id));
+        genealogy_.insert(std::make_pair(stem_id, node_sp));
+    };
+
+    typename Virus::id_type get_stem_id() const {
+        return stem_id_;
+    }
+
+    std::vector<typename Virus::id_type>
+        get_children(typename Virus::id_type const &id) const {
+        throwIfNotFound(id);
+
+        std::shared_ptr<Node> node_sp = genealogy_.find(id)->second; // strong
+        return node_sp->get_children();
+    }
+
+    std::vector<typename Virus::id_type> get_parents(typename Virus::id_type const &id) const {
+        throwIfNotFound(id);
+
+        std::shared_ptr<Node> node_sp = genealogy_.find(id)->second; // strong
+        return node_sp->get_parents();
+    }
+
+    bool exists(typename Virus::id_type const &id) const {
+        return genealogy_.find(id) != genealogy_.end(); // strong
+    }
+
+    Virus& operator[](typename Virus::id_type const &id) const {
+        throwIfNotFound(id);
+
+        std::shared_ptr<Node> node_sp = genealogy_.find(id)->second; // strong
+        return node_sp->get_virus();
+    }
+
+    void create(typename Virus::id_type const &id,
+            typename Virus::id_type const &parent_id) {
+        throwIfAlreadyCreated(id);
+        throwIfNotFound(parent_id);
+
+        std::shared_ptr<Node> node_sp = std::make_shared<Node>(id); //const
+        genealogy_.insert(std::make_pair(id, node_sp)); // strong
+
+        try {
+            connect(id, parent_id); // strong
+        } catch (...) {
+            genealogy_.erase(id); // no-throw
+        }
+    } // try-catch-reverse makes the whole function strong
+
+    void create(typename Virus::id_type const &id,
+            std::vector<typename Virus::id_type> const &parent_ids) {
+        throwIfAlreadyCreated(id);
+        for (typename Virus::id_type parent_id : parent_ids) {
+            throwIfNotFound(parent_id);
+        }
+
+        std::shared_ptr<Node> node_sp = std::make_shared<Node>(id); // const
+        genealogy_.insert(std::make_pair(id, node_sp)); // strong
+
+        for (typename Virus::id_type parent_id : parent_ids) {
+            connect(id, parent_id);
+        }
+    }
+
+    void connect(typename Virus::id_type const &child_id,
+            typename Virus::id_type const &parent_id) {
+        throwIfNotFound(child_id);
+        throwIfNotFound(parent_id);
+
+        std::shared_ptr<Node> child_sp = genealogy_.find(child_id)->second; // const
+
+        std::shared_ptr<Node> parent_sp = genealogy_.find(parent_id)->second; // const
+
+        child_sp->add_parent(parent_sp); // strong
+        try {
+            parent_sp->add_child(child_sp); // strong
+        } catch (...) {
+            child_sp->remove_parent(parent_sp); // no-throw
+            throw;
+        }
+    } // try-catch-reverse makes whole function strong
+
+    void remove(typename Virus::id_type const &id) {
+        if (id == stem_id_) {
+           throw TriedToRemoveStemVirus();
+        }
+
+        throwIfNotFound(id);
+
+        std::shared_ptr<Node> node_sp = genealogy_.find(id)->second; // const
+
+        std::vector<typename Virus::id_type> parent_ids = get_parents(id); // const?
+        for (typename Virus::id_type parent_id : parent_ids) {
+            std::shared_ptr<Node> parent_sp = genealogy_.find(parent_id)->second;
+            parent_sp->remove_child(node_sp);
+        }
+
+        std::vector<typename Virus::id_type> children_ids = get_children(id);
+        for (typename Virus::id_type child_id : children_ids) {
+            std::shared_ptr<Node> child_sp = genealogy_.find(child_id)->second;
+            child_sp->remove_parent(node_sp);
+
+            if (child_sp->get_parents().size() == 0) {
+                remove(child_sp->get_id());
+            }
+        }
+
+        genealogy_.erase(id);
+    }
 };
+
 #endif
